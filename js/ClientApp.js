12d600b8cdf34baac4102f681d8486e82cd895b3
Gustavo <andrescabral.c@gmail.com>
2017-05-06 17:01:18 -0400

    refactor(ClientApp): Color and title props added

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,10 +2,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 
 const MyTitle = React.createClass({
+  propTypes: {
+    color: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string.isRequired
+  },
   render: function () {
     return (
       React.DOM.div(null,
-        React.DOM.h1(null, 'This is my first component')
+        React.DOM.h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
       )
     )
   }
@@ -17,10 +21,10 @@ const MyFirstComponent = React.createClass({
   render: function () {
     return (
       React.DOM.div(null, [
-        MyTitleFactory(null),
-        MyTitleFactory(null),
-        MyTitleFactory(null),
-        MyTitleFactory(null)
+        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
+        MyTitleFactory({ title: 'semicolons are the worst', color: 'mediumaquamarine' }),
+        MyTitleFactory({ title: 'jklol its okay if you like semicolons', color: 'rebeccapurple' }),
+        MyTitleFactory({ title: 'i\'m out of ideas', color: 'tomato' })
       ])
     )
   }
