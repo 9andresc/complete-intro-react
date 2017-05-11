482d279278b781be88ddaa3c615ae5ddbe92a144
Gustavo <andrescabral.c@gmail.com>
2017-05-10 20:44:23 -0400

    build(webpack.config): Css and style loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = {
         include: path.resolve(__dirname, 'js'),
         loader: 'babel-loader',
         test: /\.js$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
       }
     ]
   }
